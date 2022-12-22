d12eb355bb057179a8a7c5fde9f80d1e74c3022b
Tdanks2000 <tommydanks2000@hotmail.com>
2022-12-22 02:50:19 +0000

    init

---
+++ src/parser.js
@@ -0,0 +1,110 @@
+import fs from "fs";
+import xml2js from "xml2js";
+import escape from "xml-escape";
+import got from "got";
+import gzip from "node-gzip";
+import chalk from "chalk";
+const { ungzip } = gzip;
+
+import config from "../config.json" assert { type: "json" };
+
+const { log } = console;
+
+const xmlParser = new xml2js.Parser();
+const XML_IGNORE = '<>"';
+
+const url = config.fileURL;
+const filePath = config.downloadsFilePath;
+const saveFilePath = config.saveFilePath;
+const file = config.file;
+const fileExt = config.fileExt;
+
+const fileName = (date) => `${filePath}/${file}-${date}.${fileExt}`;
+
+const getFileAndSave = async (date) => {
+  //check if file exists with today's date and if so, exit
+  if (fs.existsSync(fileName(date)))
+    return log(chalk.yellow`File already exists, moving on...`);
+
+  // fetch file
+  log(chalk.bold`fetching file...`);
+  const { body } = await got(url, {
+    responseType: "buffer",
+  });
+
+  // unzip file
+  log(chalk.yellow`unzipping file...`);
+  const unzipped = await ungzip(body);
+
+  // save file as xml
+  log(chalk.bold.green`saving file...`);
+  fs.writeFileSync(`${filePath}/${file}-${date}.${fileExt}`, unzipped, "utf8");
+};
+
+const setupDict = (date) => {
+  // Setting up dictionary
+  log(chalk.cyan`setting up dictionary...`);
+  return new Promise((resolve, reject) => {
+    //read file
+    log(chalk.green`reading file...`);
+    fs.readFile(fileName(date), (err, rawData) => {
+      if (err) {
+        log(chalk.bold.red(err));
+        process.exit(1);
+      }
+      //parse xml
+      log(chalk.green`parsing...`);
+      const data = escape(rawData.toString(), XML_IGNORE);
+
+      xmlParser.parseString(data, (parseError, obj) => {
+        if (parseError) reject(parseError);
+        else resolve(obj.JMdict.entry);
+      });
+      log(chalk.bold.green`parsed!`);
+    });
+  });
+};
+
+//save dictionary to json file
+const saveDict = async (dictionary, date) => {
+  const outputFilename = `${saveFilePath}/${file}-${date}.json`;
+
+  //check if file exists with today's date and if so, exit
+  if (fs.existsSync(outputFilename))
+    return log(chalk.green.bold`File already exists, finished...`);
+
+  await fs.writeFileSync(
+    outputFilename,
+    JSON.stringify(dictionary),
+    "utf8",
+    (err) => {
+      if (err) log(err);
+      else log(chalk.bold.green`JSON saved to ${outputFilename}`);
+    }
+  );
+};
+
+//convert date to human readable format
+const convertDate = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${day}-${month}-${year}`;
+};
+
+(async () => {
+  const date = new Date();
+  const dateString = convertDate(date);
+  const outputFilename = `${saveFilePath}/${file}-${dateString}.json`;
+
+  log(chalk.green.bold("starting..."));
+  await getFileAndSave(dateString);
+  //check if file exists with today's date and if so, exit
+  if (fs.existsSync(outputFilename))
+    return log(chalk.green.bold`File already exists, finished...`);
+  else {
+    const dictionary = await setupDict(dateString);
+    await saveDict(dictionary, dateString);
+    log(chalk.green.bold("Finished..."));
+  }
+})();
